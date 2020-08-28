5c1ed8633c0903edcbb8702a8e1dd0b975c53e43
Cmcclellan1738 <69971600+Cmcclellan1738@users.noreply.github.com>
2020-08-28 13:25:56 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,91 @@
+# Setup the game
+info.set_score(0)
+info.set_life(3)
+
+# Setup the player
+tacotruck = sprites.create(img("""
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . f f f f f f f f f f f f f f f f f f f f f f . . . . . . .
+    . . f f f f f f f f f f f f f f f f f f f f f f f f . . . . . .
+    . . 9 9 9 6 5 6 6 a 6 e 6 6 2 2 6 6 6 8 8 8 6 6 9 . . . . . . .
+    . . 9 9 8 6 5 6 6 a 6 e e 6 6 2 2 6 8 6 d d d d 9 9 . . . . . .
+    . . 9 6 8 6 6 5 6 a 6 6 e e 6 6 2 6 8 6 d d d 1 d 9 . . . . . .
+    . . 9 6 8 8 6 d d d d d d d d d d d 8 6 d d d d 1 9 . . . . . .
+    . . 9 6 6 8 6 d d d d d 1 d d d d d 6 8 d 1 d d d 9 . . . . . .
+    5 4 f 6 8 8 6 d d d d d d 1 d d d d 6 8 6 d 1 d d 9 9 . . . . .
+    5 4 f 6 8 6 6 d d d d d d d 1 d d d 6 8 6 d d 1 d d 9 . . . . .
+    5 4 f 6 8 8 6 d 1 d d d d d d 1 d d 6 8 6 6 8 6 6 6 9 9 . . . .
+    5 4 f 6 6 8 6 d d 1 d d d d d d d d 6 8 6 8 8 6 3 3 3 9 9 . . .
+    5 4 f 6 8 8 6 d d d 1 d d d d d d d 6 8 8 6 6 6 3 6 6 6 9 9 . .
+    . . 9 6 8 6 6 5 6 6 a 6 6 e 6 2 6 6 6 6 6 6 3 3 3 6 3 3 3 9 . .
+    . . 9 6 8 6 5 6 6 a 6 6 e e 6 2 2 6 6 6 3 3 3 6 6 6 3 6 6 9 . .
+    . . 9 9 6 6 5 6 a 6 6 e 6 6 6 6 2 2 6 6 3 6 6 6 3 3 3 6 3 9 . .
+    . . 9 9 9 9 9 f f f f 9 9 9 9 9 9 9 9 9 9 f f f f 9 9 9 9 9 . .
+    . . d d d d . f 1 1 f . d d d d d d d d . f 1 1 f . d d d d . .
+    . . . . . . . f 1 1 f . . . . . . . . . . f 1 1 f . . . . . . .
+    . . . . . . . f f f f . . . . . . . . . . f f f f . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+"""))
+tacotruck.set_position(20, scene.screen_height()/2)
+tacotruck.set_flag(SpriteFlag.StayInScreen, True)
+
+# Configure Player Controls
+controller.move_sprite(tacotruck, 150, 150)
+
+# Generate Enemies
+def on_update_interval():
+    taco = sprites.create(img("""
+    . . . . . . . e e e e . . . . .
+    . . . . . e e 4 5 5 5 e e . . .
+    . . . . e 4 5 6 7 6 7 6 6 e . .
+    . . . e 5 6 6 7 7 7 6 4 4 4 e .
+    . . e 5 6 7 7 6 6 4 5 5 5 5 4 .
+    . e 5 6 7 7 8 8 5 5 5 5 5 4 5 4
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e .
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . .
+    e 5 e c 5 4 5 4 5 5 5 e e . . .
+    e 5 e e 5 5 5 5 5 4 e . . . . .
+    4 5 4 e 5 5 5 5 e e . . . . . .
+    . 4 5 4 5 5 4 e . . . . . . . .
+    . . 4 4 e e e . . . . . . . . .
+    """))
+    taco.set_position(scene.screen_width(), randint(0, scene.screen_height()))
+    taco.set_velocity(-50, 0)
+game.on_update_interval(900, on_update_interval)
+
+# Shoot enemies with projectiles
+def on_button_event_a_pressed():
+    tomatoeblast = sprites.create_projectile_from_sprite(img("""
+    . . . . . . . 7 2 2 . . . . . .
+    . . . . e e e 2 7 2 7 7 . . . .
+    . . e e e 7 7 2 7 7 7 7 e e . .
+    . e e e e e 7 7 7 e 7 2 2 2 e .
+    . e e e e 2 e 7 7 7 7 2 4 2 e .
+    e e e e 2 2 7 7 2 2 7 7 2 2 2 e
+    e e e 2 2 2 7 2 2 2 2 4 4 2 2 e
+    e e e 2 2 2 2 2 4 2 2 2 2 2 2 e
+    e e e 2 2 2 4 4 2 2 2 2 2 2 2 e
+    e e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+    e e e 2 2 2 2 2 2 2 2 2 2 4 2 e
+    . e e e 2 2 2 2 2 2 2 2 2 4 e .
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e .
+    . . 2 e e 2 2 2 2 2 4 4 2 e . .
+    . . . 2 2 e e 4 4 4 2 e e . . .
+    . . . . . 2 2 e e e e . . . . .
+    """), tacotruck, 50, 0)
+    
+controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, on_button_event_a_pressed)
